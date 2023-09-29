64f81435615cce2158cfd30d5cca4092d9097d63
Jhonna <jhonna.somarriba@gmail.com>
2023-09-29 15:39:01 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <router-view />
+</template>
+
+<script>
+import AboutPage from "@/modules/pokemon/pages/AboutPage";
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
