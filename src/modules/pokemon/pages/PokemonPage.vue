7542fd8d7578fb3fae435a44db55a4d8f5912069
Jhonna <jhonna.somarriba@gmail.com>
2023-10-23 11:09:54 -0600

    Http requests and redirects

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -2,6 +2,10 @@
   <h1>
     Pokemon: <span>#{{ id }}</span>
   </h1>
+  <div v-if="pokemon">
+    <h2>{{ pokemon.name }}</h2>
+    <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+  </div>
 </template>
 
 <script>
@@ -15,6 +19,7 @@ export default {
   data() {
     return {
       // id:this.route.params.id
+      pokemon: null,
     };
   },
 
@@ -22,6 +27,25 @@ export default {
     // const { id } = this.$route.params;
     // console.log(id);
     // this.id = id;
+    this.getPokemon();
+  },
+  methods: {
+    async getPokemon() {
+      try {
+        const pokemon = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${this.id}`
+        ).then((res) => res.json());
+        this.pokemon = pokemon;
+      } catch (error) {
+        this.$router.push("/");
+        console.log("No hay nada aqui");
+      }
+    },
+  },
+  watch: {
+    id() {
+      this.getPokemon();
+    },
   },
 };
 </script>
