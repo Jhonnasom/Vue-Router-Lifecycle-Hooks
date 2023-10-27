cc41dd68ea5fe4acb35005a3a7efa321b21871d3
Jhonna <jhonna.somarriba@gmail.com>
2023-10-26 21:44:30 -0600

    second layout dbz and children routes

--- src/router/router.js
+++ src/router/router.js
@@ -44,7 +44,39 @@ const routes = [
       },
       {
         path: "",
-        redirect: { name: "pokemon-about" },
+        redirect: { name: "pokemon-about  " },
+      },
+    ],
+  },
+
+  // DBZ Layout
+  // layout:path => /dbz
+  // path children path:characters
+  {
+    path: "/dbz",
+    name: "dbz",
+    component: () =>
+      import(
+        /*webpackChunkName:"DragonBallLayout"*/ "../modules/dbz/layouts/DragonBallLayout"
+      ),
+    children: [
+      {
+        path: "characters",
+        name: "dbz-characters",
+        component: () =>
+          import(
+            /*webpackChunkName:"DbzCharacters"*/ "../modules/dbz/pages/Characters"
+          ),
+      },
+      {
+        path: "about",
+        name: "dbz-about",
+        component: () =>
+          import(/*webpackChunkName:"DbzAbout"*/ "../modules/dbz/pages/About"),
+      },
+      {
+        path: "",
+        redirect: { name: "dbz-characters" },
       },
     ],
   },
